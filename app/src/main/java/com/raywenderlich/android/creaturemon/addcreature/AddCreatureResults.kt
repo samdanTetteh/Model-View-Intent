5b958e453adf0652013aee6aeb57989485fd2707
Samdan <samdan@ijikod.com>
2021-02-07 07:09:55 +0000

    implemented add creature results

---
+++ app/src/main/java/com/raywenderlich/android/creaturemon/addcreature/AddCreatureResults.kt
@@ -0,0 +1,43 @@
+package com.raywenderlich.android.creaturemon.addcreature
+
+import com.raywenderlich.android.creaturemon.mviBase.MviResult
+
+sealed class AddCreatureResults : MviResult {
+    sealed class AvatarResult : AddCreatureResults() {
+        object Processing : AvatarResult()
+        data class Success(val drawable: Int) : AvatarResult()
+        data class Failure(val error: Throwable) : AvatarResult()
+    }
+
+    sealed class NameResult : AddCreatureResults() {
+        object Processing : NameResult()
+        data class Success(val name: String) : NameResult ()
+        data class Failure(val error: Throwable) : NameResult()
+    }
+
+    sealed class IntelligenceResult : AddCreatureResults() {
+        object Processing : IntelligenceResult()
+        data class Success(val intelligence : Int) : IntelligenceResult()
+        data class Failure(val error : Int) : IntelligenceResult()
+    }
+
+    sealed class StrengthResult : AddCreatureResults() {
+        object Processing : StrengthResult()
+        data class Success(val strength : Int) : StrengthResult()
+        data class Failure(val error : Int) : StrengthResult()
+    }
+
+    sealed class EnduranceResult : AddCreatureResults() {
+        object Processing : EnduranceResult()
+        data class Success(val endurance : Int) : EnduranceResult()
+        data class Failure(val error : Int) : EnduranceResult()
+    }
+
+    sealed class SaveCreatureResult : AddCreatureResults() {
+        object Processing: SaveCreatureResult()
+        object Success : SaveCreatureResult()
+        data class Failure(val error: Throwable) : SaveCreatureResult()
+    }
+
+
+}
